0704d18fefd14ec7839efa6631a26963c7f84b69
umarkulov-s <sonwesley79@gmail.com>
2026-02-16 21:04:16 +0500

    gg

--- OneDrive/Desktop/Vs code projects/numbers/scipt.js
+++ OneDrive/Desktop/Vs code projects/numbers/scipt.js
@@ -6,15 +6,37 @@
 // num--;
 
 // console.log(num);
-let gap = prompt("Uzun gap kiriting:").trim();
-let harf = prompt("Qaysi harfni sanaymiz?").trim();
+// let gap = prompt("Uzun gap kiriting:").trim();
+// let harf = prompt("Qaysi harfni sanaymiz?").trim();
 
-let count = 0;
+// let count = 0;
 
-for (let i = 0; i < gap.length; i++) {
-    if (gap[i].toLowerCase() == harf.toLowerCase()) {
-        count++;
+// for (let i = 0; i < gap.length; i++) {
+//     if (gap[i].toLowerCase() == harf.toLowerCase()) {
+//         count++;
+//     }
+// }
+
+// alert(harf + " - " + count + " marta yozilgan");
+
+
+
+var gap = prompt("Uzun gap kiriting:").trim().toLowerCase();
+
+var hisob = {};
+
+for (var i = 0; i < gap.length; i++) {
+    var s = gap[i];
+
+    if (s !== " ") {
+        if (hisob[s]) {
+            hisob[s]++;
+        } else {
+            hisob[s] = 1;
+        }
     }
 }
 
-alert(harf + " - " + count + " marta yozilgan");
+for (let harf in hisob) {
+    console.log(harf + " - " + hisob[harf] + " ta") ;
+}
